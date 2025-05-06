04484121eac5c64045f53edf1994f0f501b032e1
accapello4life <fred.isaac2012@gmail.com>
2025-05-06 14:35:18 +0000

    Initial commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
@@ -9,16 +10,33 @@ app.use(cors());
 app.use(express.json());
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/loveliberia', {
+mongoose.connect('mongodb+srv://fredisaac2012:ATvCCwKBqBtRKAfE@cluster0.1ukcfz2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
+// JWT Middleware
+const authenticateToken = (req, res, next) => {
+  const token = req.headers['authorization']?.split(' ')[1];
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+
 // Basic route
 app.get('/', (req, res) => {
   res.send('LoveLiberia API');
+});
+
+// Protected route example
+app.get('/protected', authenticateToken, (req, res) => {
+  res.json({ message: 'Protected route accessed', user: req.user });
 });
 
 const PORT = process.env.PORT || 5000;
